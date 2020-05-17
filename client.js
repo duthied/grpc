8158435abdf25bf3e2ab6a2817167f5f665747a5
Devlon Duthie <duthied@gmail.com>
2020-05-17 12:56:22 -0400

    Basic functionality

---
+++ client.js
@@ -0,0 +1,31 @@
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+
+const PROTO_PATH = 'chat.proto'
+const SERVER_URI = '0.0.0.0:50051'
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH)
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+
+const client = new protoDescriptor.ChatService(SERVER_URI, grpc.credentials.createInsecure())
+
+const user = 'Devlon'
+const sendMessageIntervalInMS = 2000
+
+const chatStream = client.joinChat({ user })
+
+chatStream.on('data', data => {
+  const { message, user } = data
+
+  console.log(`New message from ${user}: ${message}`)
+})
+
+let messageNumber = 0
+
+setInterval(() => {
+  client.sendMessage({ message: `New message no. ${messageNumber++}`, user }, (error, response) => {
+    if (error) {
+      console.log(error)
+    }
+  })
+}, sendMessageIntervalInMS)
--- server.js
+++ server.js
@@ -4,15 +4,43 @@ const protoLoader = require('@grpc/proto-loader')
 const PROTO_PATH = 'chat.proto'
 const SERVER_URI = '0.0.0.0:50051'
 
-const usersInChat = []
+let usersInChat = []
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH)
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
 
-// handlers will go here
+const joinChat = call => {
+  console.log(`User ${call.request.user} has joined`)
+
+  call.on('cancelled', () => {
+    usersInChat = usersInChat.filter(user => user !== call)
+  })
+
+  usersInChat.push(call)
+}
+
+const sendMessage = (call, callback) => {
+  const { message } = call.request
+
+  if (!message) {
+    return callback(new Error('You must provide a non-empty message.'))
+  }
+
+  const messageToSend = {
+    ...call.request,
+    timestampe: Math.floor(new Date().getTime() / 1000),
+  }
+
+  usersInChat.forEach(user => user.write(messageToSend))
+
+  callback(null, {})
+}
 
 const server = new grpc.Server()
-server.addService(protoDescriptor.ChatService.service, {})
+server.addService(protoDescriptor.ChatService.service, {
+  joinChat,
+  sendMessage,
+})
 server.bind(SERVER_URI, grpc.ServerCredentials.createInsecure())
 
 server.start()
